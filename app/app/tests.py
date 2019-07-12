1f530cb148744adb998d85e31650168369060389
Al-Bsharat <qaisbsharat@onedrive.web.id>
2019-07-12 16:03:00 +0300

    Removed Dummy Tests And Starting With CoreApp

--- app/app/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from app.calc import add
-
-
-class CalcTests(TestCase):
-
-    def test_add_whenCalled_returnSummationTwoNumbers(self):
-        """Test that two numbers are added together"""
-        self.assertEqual(add(4, 8), 12)
